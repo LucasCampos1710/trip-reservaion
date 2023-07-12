d3846f6f171eb15f2e1fc455beaa70e56f150a96
Lucas <lucas@apx_managed.vanilla>
2023-07-12 15:46:35 -0300

    feat: add imageComponentAndAddRecommendedTrips

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,23 +15,24 @@ export const Header = () => {
   const handleMenuClick = () => setMenuIsOpen(!menusIsOpen)
 
   return (
-    <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between">
-      <Image 
-      width={183}
-      height={32}
-      src="/Logo.svg"
-      alt="Trips Reservation"
-      />
+    <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
+      <div className="relative h-[32px] w-[183px]" >
+        <Image 
+        src="/Logo.svg"
+        alt="Trips Reservation"
+        fill
+        />
+      </div>
 
       {status === "unauthenticated" && (
         <button className="text-primary text-sm font-semibold" onClick={handleLoginClick}>Login</button>
       )}
 
       {status === "authenticated" && data.user &&(
-        <div className="flex items-center gap-5 border-gray-300 border border-solid rounded-full p-2 px-3 relative">
+        <div className="flex items-center gap-3 border-gray-300 border border-solid rounded-full p-2 px-3 relative">
             <AiOutlineMenu size={24} onClick={handleMenuClick} className="cursor-pointer"/>
 
-            <Image height={40} width={40} src={data.user.image!} alt={data.user.name!} className="rounded-full shadow-md"/>
+            <Image height={35} width={35} src={data.user.image!} alt={data.user.name!} className="rounded-full shadow-md"/>
 
             {menusIsOpen && (
               <div className="absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
