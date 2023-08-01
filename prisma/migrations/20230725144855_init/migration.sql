75c810a27b678070e02a5c7acb2479f2d8b3bdb8
lucascampos1710 <soluxcreative@gmail.com>
2023-08-01 15:27:50 -0300

    feat: add location description

---
+++ prisma/migrations/20230725144855_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
